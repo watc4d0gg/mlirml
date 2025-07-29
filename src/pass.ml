0c23243f336a755a2486b851469ec289c0744d8e
watc4d0gg <tomasek.matej@gmail.com>
2025-07-29 13:43:58 +0100

    Fix ExternalPass creation

--- src/pass.ml
+++ src/pass.ml
@@ -19,47 +19,44 @@ module ExternalPass = struct
 
   let create id name arg description op dialects ~on_construct ~on_destruct ~on_clone ~init ~run =
     let dialect_array = dialects |> List.map (fun dialect -> dialect#raw) |> CArray.of_list MlirDialectHandle.t in
-    let callbacks = Ctypes.make
-      ?finalise:(Some (fun value ->
-        let construct_funptr =
-          coerce
-            (Foreign.funptr (ptr void @-> returning void))
-            (static_funptr (ptr void @-> returning void))
-            (fun _ -> on_construct ())
-        in
-        let destruct_funptr =
-          coerce
-            (Foreign.funptr (ptr void @-> returning void))
-            (static_funptr (ptr void @-> returning void))
-            (fun _ -> on_destruct ())
-        in
-        let clone_funptr =
-          coerce
-            (Foreign.funptr (ptr void @-> returning (ptr void)))
-            (static_funptr (ptr void @-> returning (ptr void)))
-            (fun _ -> on_clone (); null)
-        in
-        let init_funptr =
-          coerce
-            (Foreign.funptr (MlirContext.t @-> ptr void @-> returning MlirLogicalResult.t))
-            (static_funptr (ptr MlirContext.t @-> ptr void @-> returning (ptr MlirLogicalResult.t)))
-            (fun ctx _ -> match init (Context.from_raw ctx) with
-            | LogicalResult.Success -> mlir_logical_result_success ()
-            | LogicalResult.Failure -> mlir_logical_result_failure ())
-        in
-        let run_funptr =
-          coerce
-            (Foreign.funptr (MlirOperation.t @-> MlirExternalPass.t @-> ptr void @-> returning void))
-            (static_funptr (ptr MlirOperation.t @-> ptr MlirExternalPass.t @-> ptr void @-> returning void))
-            (fun op pass _ -> run (Operation.from_raw op) (from_raw pass))
-        in
-        setf value MlirExternalPassCallbacks.construct construct_funptr;
-        setf value MlirExternalPassCallbacks.destruct destruct_funptr;
-        setf value MlirExternalPassCallbacks.clone clone_funptr;
-        setf value MlirExternalPassCallbacks.initialize init_funptr;
-        setf value MlirExternalPassCallbacks.run run_funptr;
-        print_endline "created callbacks!";))
-        MlirExternalPassCallbacks.t in
+    let callbacks = Ctypes.make MlirExternalPassCallbacks.t in
+    let construct_funptr =
+      coerce
+        (Foreign.funptr (ptr void @-> returning void))
+        (static_funptr (ptr void @-> returning void))
+        (fun _ -> on_construct ())
+    in
+    let destruct_funptr =
+      coerce
+        (Foreign.funptr (ptr void @-> returning void))
+        (static_funptr (ptr void @-> returning void))
+        (fun _ -> on_destruct ())
+    in
+    let clone_funptr =
+      coerce
+        (Foreign.funptr (ptr void @-> returning (ptr void)))
+        (static_funptr (ptr void @-> returning (ptr void)))
+        (fun _ -> on_clone (); null)
+    in
+    let init_funptr =
+      coerce
+        (Foreign.funptr (MlirContext.t @-> ptr void @-> returning MlirLogicalResult.t))
+        (static_funptr (ptr MlirContext.t @-> ptr void @-> returning (ptr MlirLogicalResult.t)))
+        (fun ctx _ -> match init (Context.from_raw ctx) with
+        | LogicalResult.Success -> mlir_logical_result_success ()
+        | LogicalResult.Failure -> mlir_logical_result_failure ())
+    in
+    let run_funptr =
+      coerce
+        (Foreign.funptr (MlirOperation.t @-> MlirExternalPass.t @-> ptr void @-> returning void))
+        (static_funptr (ptr MlirOperation.t @-> ptr MlirExternalPass.t @-> ptr void @-> returning void))
+        (fun op pass _ -> run (Operation.from_raw op) (from_raw pass))
+    in
+    setf callbacks MlirExternalPassCallbacks.construct construct_funptr;
+    setf callbacks MlirExternalPassCallbacks.destruct destruct_funptr;
+    setf callbacks MlirExternalPassCallbacks.clone clone_funptr;
+    setf callbacks MlirExternalPassCallbacks.initialize init_funptr;
+    setf callbacks MlirExternalPassCallbacks.run run_funptr;
     mlir_create_external_pass
       id#raw
       (match name with
@@ -76,7 +73,7 @@ end
 
 module OpPassManager = struct
   class t raw = object (self)
-    method nested_under = mlir_op_pass_manager_get_nested_under self#raw
+    method nested_under op = mlir_op_pass_manager_get_nested_under self#raw (mlir_string_ref_create_from_cstring op) |> new t
     method add_pass pass = mlir_op_pass_manager_add_owned_pass self#raw pass
     method add_pipeline pipeline =
       let error_message = ref String.empty in
@@ -109,7 +106,7 @@ module PassManager = struct
       mlir_pass_manager_enable_irprinting self#raw print_before_all print_after_all print_module_scope print_after_only_on_change print_after_only_on_failure flags#raw (mlir_string_ref_create_from_cstring tree_printing_path)
     method enable_verifier enable = mlir_pass_manager_enable_verifier self#raw enable
     method as_op_pass_manager = mlir_pass_manager_get_as_op_pass_manager self#raw |> OpPassManager.from_raw
-    method nested_under = mlir_pass_manager_get_nested_under self#raw
+    method nested_under op = mlir_pass_manager_get_nested_under self#raw (mlir_string_ref_create_from_cstring op) |> OpPassManager.from_raw
     method add_pass pass = mlir_pass_manager_add_owned_pass self#raw pass
     method raw = raw
   end
